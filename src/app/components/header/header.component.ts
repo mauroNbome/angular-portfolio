ff9f851fd9bc182858e570920ca294ad6a3a77b1
mauroR <mauro.rios@syloper.com>
2022-02-17 00:21:48 -0300

    dark mode button and ls

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -8,16 +8,19 @@ import { SidenavService } from '.././sidenav/sidenav.service';
 })
 export class HeaderComponent implements OnInit {
   lang: string;
+  colorMode: string = 'light';
   constructor(private SidenavService: SidenavService) {}
 
   ngOnInit(): void {
     this.setLangFromLS();
+    this.setColorModeFromLS();
   }
 
   toggleSidenav(): void {
     this.SidenavService.isOpened = !this.SidenavService.isOpened;
   }
 
+  // Handling of languages.
   changeLang(event: string): void {
     this.lang = event;
     this.saveLangToLS();
@@ -29,5 +32,29 @@ export class HeaderComponent implements OnInit {
 
   saveLangToLS(): void {
     localStorage.setItem('lang', this.lang);
+  }
+
+  // Handling of darkmode
+
+  toggleColorMode(): void {
+    if (this.colorMode === 'dark') {
+      this.colorMode = 'light';
+    } else {
+      this.colorMode = 'dark';
+    }
+    this.saveColorModeToLS();
+  }
+
+  changeColorMode(event: string): void {
+    this.colorMode = event;
+    this.saveColorModeToLS();
+  }
+
+  setColorModeFromLS(): void {
+    this.colorMode = localStorage.getItem('cm') || 'dark';
+  }
+
+  saveColorModeToLS(): void {
+    localStorage.setItem('cm', this.colorMode);
   }
 }
