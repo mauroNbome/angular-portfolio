e67ffe0713c08c4f1bea31a1c28f394b4e3f5685
mauroR <mauro.rios@syloper.com>
2022-03-20 23:47:57 -0300

    home layout init, large screen done, some responsiveness in md

--- src/app/layouts/home/home.component.ts
+++ src/app/layouts/home/home.component.ts
@@ -3,13 +3,49 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  findWithMe = [
+    {
+      alt: 'linkedin-logo',
+      goto: 'http://google.com',
+      logo: '../../../assets/icons/logo-linkedin.svg',
+    },
+    {
+      alt: 'facebook-logo',
+      goto: 'http://google.com',
+      logo: '../../../assets/icons/logo-facebook.svg',
+    },
+    {
+      alt: 'linkedin-logo',
+      goto: 'http://google.com',
+      logo: '../../../assets/icons/logo-instagram.svg',
+    },
+  ];
 
-  constructor() { }
+  bestSkills = [
+    {
+      alt: 'angular-logo',
+      goto: 'https://angular.io/',
+      logo: '../../../assets/icons/logo-angular.svg',
+    },
+    {
+      alt: 'ionic-logo',
+      goto: 'https://ionicframework.com/',
+      logo: '../../../assets/icons/logo-ionic.svg',
+    },
+    {
+      alt: 'nodejs-logo',
+      goto: 'https://nodejs.org/',
+      logo: '../../../assets/icons/logo-nodejs.svg',
+    },
+  ];
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  goToLink(url: string) {
+    window.open(url, '_blank');
   }
-
 }
