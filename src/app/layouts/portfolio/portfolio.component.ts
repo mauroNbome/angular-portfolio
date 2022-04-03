787b9dc71ca5891f564b51b99650c58108d54c92
mauroR <mauro.rios@syloper.com>
2022-04-03 03:37:53 -0300

    portfolio page almost done

--- src/app/layouts/portfolio/portfolio.component.ts
+++ src/app/layouts/portfolio/portfolio.component.ts
@@ -3,13 +3,86 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.scss']
+  styleUrls: ['./portfolio.component.scss'],
 })
 export class PortfolioComponent implements OnInit {
+  mockProjects = [
+    {
+      id: 0,
+      picture: '',
+      type: 'Mobile App',
+      likes: 115,
+      alreadyLiked: false,
+      title: 'Mobile App landing design & Services',
+    },
 
-  constructor() { }
+    {
+      id: 1,
+      picture: '',
+      type: 'Mobile App',
+      likes: 115,
+      alreadyLiked: false,
+      title: 'Mobile App landing design & Services',
+    },
 
-  ngOnInit(): void {
+    {
+      id: 2,
+      picture: '',
+      type: 'Mobile App',
+      likes: 115,
+      alreadyLiked: false,
+      title: 'Mobile App landing design & Services',
+    },
+
+    {
+      id: 3,
+      picture: '',
+      type: 'Mobile App',
+      likes: 115,
+      alreadyLiked: true,
+      title: 'Mobile App landing design & Services',
+    },
+
+    {
+      id: 4,
+      picture: '',
+      type: 'Mobile App',
+      likes: 115,
+      alreadyLiked: false,
+      title: 'Mobile App landing design & Services',
+    },
+    {
+      id: 5,
+      picture: '',
+      type: 'Mobile App',
+      likes: 115,
+      alreadyLiked: false,
+      title: 'Mobile App landing design & Services',
+    },
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  addToFav(item) {
+    console.log(item);
+
+    console.log('in');
+    this.mockProjects.forEach((el) => {
+      if (item.id === el.id) {
+        if (!item.alreadyLiked) {
+          el.likes++;
+          el.alreadyLiked = true;
+        } else {
+          el.likes--;
+          el.alreadyLiked = false;
+        }
+      }
+    });
   }
 
+  alreadyLiked(status: boolean) {
+    return status ? 'favorite' : 'favorite_border';
+  }
 }
